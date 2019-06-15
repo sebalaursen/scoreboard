36b896ffea43c9d8108fb1153f39912e4eb82fc0
sebalaursen <sebalaursen@gmail.com>
2019-06-15 15:20:46 +0300

    First commit

---
+++ scoreboard/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scoreboard
+//
+//  Created by Sebastian on /15/6/19.
+//  Copyright © 2019 Sebastian Laursen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
