5ffd23e9b9b50f897a2058bd768d7761d99783c2
sebalaursen <sebalaursen@gmail.com>
2019-06-16 09:37:44 +0300

    Tap to add point

--- scoreboard/Controller/MainViewController.swift
+++ scoreboard/Controller/MainViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftScore: UILabel!
+    @IBOutlet weak var rightScore: UILabel!
+    private var currentLeftScore = 0
+    private var currentRightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addGestures()
+    }
+    
+    private func addGestures() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        let tap1 = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        leftScore.addGestureRecognizer(tap)
+        rightScore.addGestureRecognizer(tap1)
+        leftScore.isUserInteractionEnabled = true
+        rightScore.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc private func handleTap(sender: UITapGestureRecognizer) {
+        guard let score = sender.view as! UILabel? else {
+            return
+        }
+        var current = Int(score.text!)!
+        current += 1
+        score.text = "\(current)"
     }
 
 
